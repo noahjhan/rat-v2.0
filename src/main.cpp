00227f75329f599398462f0cb5d3153cea3749a5
noahjhan <noahjhan11@gmail.com>
2025-08-15 17:37:20 -0500

    simplified validation structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,9 @@
+#include "lexer.hpp"
 #include "rat_source.hpp"
 
 int main() {
-  try {
-    const std::string filename = "../data/empty.rat";
-    RatSource source(filename);
-    while (source.peekChar() != EOF) {
-      source.readChar();
-      source.reverse();
-      unsigned int line = source.getLineNum();
-      unsigned int col = source.getColNum();
-      std::cout << source.readChar() << " - line: " << line << " col: " << col
-                << std::endl;
-      source.advanceWhitespace();
-    }
-  } catch (std::exception &e) {
-    std::cerr << e.what() << std::endl;
-  }
+  std::string filename = "../data/empty.rat";
+  RatSource source(filename);
+  Lexer lexer(source);
   return 0;
 }
